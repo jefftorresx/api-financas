1f6226bdeef6ebb26c3916f4abbd030943797ac4
Jeffrey Richardson <jeff.dsv@gmail.com>
2025-03-07 16:42:53 -0400

    Estrutura incial do projeto com Spring Boot.

---
+++ src/main/java/com/financas/pessoais/ApiDeFinancasPessoaisApplication.java
@@ -0,0 +1,13 @@
+package com.financas.pessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeFinancasPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeFinancasPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financas/pessoais/ApiDeFinancasPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financas.pessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeFinancasPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
