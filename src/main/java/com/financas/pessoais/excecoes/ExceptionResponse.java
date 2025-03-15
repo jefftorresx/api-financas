b28de424e406f89fd177b52194a8663febf01807
Jeffrey Richardson <jeff.dsv@gmail.com>
2025-03-14 21:51:27 -0400

    Adiciona a classe ExceptionResponse para padronizar respostas de erro

---
+++ src/main/java/com/financas/pessoais/excecoes/ExceptionResponse.java
@@ -0,0 +1,27 @@
+package com.financas.pessoais.excecoes;
+
+
+import org.springframework.http.HttpStatus;
+
+public class ExceptionResponse {
+    private HttpStatus status;
+    private String message;
+
+    public ExceptionResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
